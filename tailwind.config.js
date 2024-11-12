d947ce2c7a493a100c2f2bca15ae15af20054ac1
Maathiis <mathisperrault@hotmail.fr>
2024-11-12 20:49:17 +0100

    contact / blog 1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 tailwind.config = {
-    theme: {
-      extend: {
-        colors: {
-          clifford: '#da373d',
-          font: '#F1FAEE',
-        }
-      }
-    }
-  }
+  theme: {
+    extend: {
+      colors: {
+        clifford: "#da373d",
+        lightdark: "#242223",
+        font: "#F1FAEE",
+      },
+    },
+  },
+};
